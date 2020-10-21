c96a17cd10a898021fa17d762b56f032b49bb2d7
Milan938 <23mboga@athenian.org>
2020-10-21 19:26:51 +0000

    Not done

--- my_code.py
+++ my_code.py
@@ -1,3 +1,32 @@
 # Collaborators (including web sites where you got help: (enter none if you didn't need help)
 #  
 
+team_list = {}
+
+print("Please follow the prompts to input your favorite sports team")
+print("Please input the city or state your favorite sports team is from")
+city = input()
+print("Next, please enter the name of this team")
+name = input()
+
+team_list.update({city: name})
+
+print("Please input the city or state your favorite sports team is from")
+city2 = input()
+print("Next, please enter the name of this team")
+name2 = input()
+
+team_list.update({city2: name2})
+
+print("Please input the city or state your favorite sports team is from")
+city3 = input()
+print("Next, please enter the name of this team")
+name3 = input()
+
+team_list.update({city3: name3})
+
+
+
+
+
+print(team_list)
