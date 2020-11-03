50aa7ce1c10fd337a273efdfe7df9e3984cad164
Milan938 <23mboga@athenian.org>
2020-11-03 23:23:23 +0000

    Finished:)

--- my_code.py
+++ my_code.py
@@ -3,30 +3,20 @@
 
 team_list = {}
 
-print("Please follow the prompts to input your favorite sports team")
-print("Please input the city or state your favorite sports team is from")
-city = input()
-print("Next, please enter the name of this team")
-name = input()
+while True:
+    print("Please follow the prompts to input your favorite sports team")
+    print("Please input the city or state your favorite sports team is from")
+    city = input()
+    print("Next, please enter the name of this team")
+    name = input()
 
-team_list.update({city: name})
+    team_list.update({city: name})
+    question = input("Would you like to stop? Enter Yes or No: ")
+    if question == "Yes":
+        break
 
-print("Please input the city or state your favorite sports team is from")
-city2 = input()
-print("Next, please enter the name of this team")
-name2 = input()
-
-team_list.update({city2: name2})
-
-print("Please input the city or state your favorite sports team is from")
-city3 = input()
-print("Next, please enter the name of this team")
-name3 = input()
-
-team_list.update({city3: name3})
-
-
-
-
-
+   
 print(team_list)
+
+for key, value in team_list.items():
+    print(key, value)
